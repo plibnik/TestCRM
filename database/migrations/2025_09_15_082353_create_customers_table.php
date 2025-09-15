ee6f0ca901f0f9fb1f6ad8551a2edc77670a9d47
Dmytro Vozniuk <plibnik@gmail.com>
2025-09-15 11:50:00 +0300

    first Migration added: "customers" table. Link to "companies" is not yet operational. Tested in php artisan migrate, works OK.

---
+++ database/migrations/2025_09_15_082353_create_customers_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        // "Клієнт" з ТЗ буде "Customer", щоб не плутати зі службовими сенсами слова "клієнт" (БД тощо)
+        // ТЗ: ПІБ; телефон; email; компанія (**ЦЕ БУДЕ ЗОВН. КЛЮЧ, ЗРОБИТИ ПІСЛЯ ТАБЛИЦІ "company")
+        // ТЗ: примітка
+
+        Schema::create('customers', function (Blueprint $table) {
+            $table->id();                          // індекс для лінкування, ** додати унікальний?
+                                                   // artisan migrate створив id з автоінкрементом, але не унікальний
+            $table->string('full_name');    //ПІБ клієнта з Т.З., 256 вистачить
+            $table->string('phone_number'); //потім зробити з валідацією, 256 вистачить
+            $table->string('email_address');//теж валідацію потім
+            // $table->foreignId('company_id')->constrained('company'); // РОЗКОМЕНТУВАТИ, коли створю табл. компаній
+            $table->text('comment');        //тут довший, мало що там можуть тримати
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('customers');
+    }
+};
