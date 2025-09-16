981ce076e846bfdf3677012a1bf63e1f5899ca8f
Dmytro Vozniuk <plibnik@gmail.com>
2025-09-16 14:10:29 +0300

    changed amount() in OrderFactory to float w/2 decimals and comment to faker, 2 words (like product name -- absent in the order description in spec)

--- database/factories/OrderFactory.php
+++ database/factories/OrderFactory.php
@@ -22,11 +22,11 @@ class OrderFactory extends Factory
         $possible_status_list = ['new', 'working','completed','cancelled'];
 
         return [
-            'amount'=>random_int(1,100), // of course it would not be int, the DB value is BigInteger
+            'amount'=>fake()->randomFloat(2, 100, 10000), // of course it would not be int, the DB value is BigInteger
             'status'=> $possible_status_list[rand(0,count($possible_status_list)-1)],
             'customer_id'=>Customer::inRandomOrder()->first(), // computationally inefficient but great??
             'manager_id'=>Manager::inRandomOrder()->first(), // I LOVE this feature!
-            'comment'=>'',
+            'comment'=>fake()->sentence(2),
             'creation_date'=>Carbon::now()
             //
         ];
