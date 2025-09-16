329fb09b50e86a72b0642fc5e18e0af65716abb9
Dmytro Vozniuk <plibnik@gmail.com>
2025-09-16 14:55:48 +0300

    added HasMany -- Company has many Clients (no actual use now), Manager has many Orders (TZ: Manager can see only one's own Orders; Report: Total order amount per selected manager)

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -4,9 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Company extends Model
 {
     use HasFactory;
     protected $table = 'companies'; //not necessary here, but what if?
+
+    public function customers():HasMany
+    {
+        return $this->hasmany(Customer::class);
+    }
 }
--- app/Models/Manager.php
+++ app/Models/Manager.php
@@ -4,9 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Manager extends Model
 {
     use HasFactory;
     protected $table = 'managers'; //not necessary here, but what if?
+
+    public function orders(): HasMany
+    {
+        return $this->hasMany(Order::class);
+    }
 }
